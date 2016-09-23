16ceb6cdabb0a607d3e2ca96c5d2f7dcc7dd61c9
yhnlbyhnd <yhnlbyhnd@gmail.com>
2016-09-23 13:16:58 -0500

    Create routes.php

---
+++ app/Http/routes.php
@@ -0,0 +1,15 @@
+Route::get('/', function () {
+   return 'Hello World';
+});
+
+Route::post('foo/bar', function () {
+   return 'Hello World';
+});
+
+Route::put('foo/bar', function () {
+   //
+});
+
+Route::delete('foo/bar', function () {
+   //
+});
